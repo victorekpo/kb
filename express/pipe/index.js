9a04d013372fb0892c6614acbb601071adc0be83
Vic <victor.ekpo@gmail.com>
2022-09-28 21:42:27 -0500

    update

--- express/pipe/index.js
+++ express/pipe/index.js
@@ -1,5 +1,7 @@
 //Require Express module
 const express = require('express');
+const request = require('request');
+const path = require('path');
 
 //Define port for local web server
 let port = 5000;
@@ -11,6 +13,39 @@ const app = express();
 app.get('/', function(req,res){
  res.send('Hello World');
 })
+
+app.get('/pipe', function(req,res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Transfer-Encoding': 'chunked'
+  });
+  
+  const pipe = req.pipe(request.get('http://localhost:5000/data'));
+  const response = [];
+  let count=0;
+  
+  pipe.on('data', function(chunk) {
+    response.push(chunk);
+    if(response.length > count){
+      res.write(chunk);
+      count++;
+    }
+    //res.end();
+  });
+  
+  pipe.on('end', function() {
+    const res2 = Buffer.concat(response);
+    console.log(res2);
+    res.end();
+  });
+});
+
+app.get('/data', function(req,res) {
+  // res.json({a:1,b:2});
+  res.header("Content-Type",'application/json');
+  res.sendFile(path.join(__dirname, 'data.js'));
+ // res.sendFile(path.join(__dirname, 'data2.js'));
+});
 
 //Listen on the defined port
 app.listen(port, function(){
